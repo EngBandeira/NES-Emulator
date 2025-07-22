a9423b44b2221bfeb774470fc0c8b7f79463c441
Eduardo Bandeira <eduardo.bandeira.eng@outlook.com>
2025-07-22 18:31:38 -0300

    Initial file treatment

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,31 @@
 
 #include "utils.hpp"
 char sing [4] = {0x7f ,0x45, 0x4c ,0x46};
+unsigned int prgRomDataLenght;
+unsigned int chrRomDataLenght;
+
+
 int
-main (int argv,const char **args)
+main(int argc, char const *argv[])
 {
-  if(argv < 2){
+  if(argc < 2){
     sendError("NOT ENOUGH ARGUMENTS");
     exit(1);
   }
   unsigned int fileLenght = 0;
-  char *fileBuffer = readFile(args[1],&fileLenght);
-  if(fileBuffer[0] != sing[0] ||fileBuffer[0] != sing[0] ||fileBuffer[0] != sing[0] ||fileBuffer[0] != sing[0]){
+  char *fileBuffer = readFile(argv[1],&fileLenght);
+  if(fileBuffer[0] != sing[0] ||fileBuffer[1] != sing[1] ||fileBuffer[2] != sing[2] ||fileBuffer[3] != sing[3]){
     sendError("WRONG FILE TYPE");
     exit(1);
   }
-  printf("\n");
+  prgRomDataLenght = 16384 * fileBuffer[4];
+  chrRomDataLenght = 8192 * fileBuffer[5];
+  if(fileBuffer[6] & 0b100){
+     sendError("Cannot manage traine");
+    exit(1);
+  }
+  // 16  <-> 16 + prgRomDataLenght -1
+  unsigned int index = 16;
+
   // printf("%x %d\n",fileBuffer, fileLenght);
 }
